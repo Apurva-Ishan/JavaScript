c54bdb05ffb3845ce3510b784a4d9b2db117d584
Apurva Ishan <apurva.0072@gmail.com>
2026-01-14 05:56:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "abcsd"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" -> 33
+// "33ab" -> NaN
+// true -> 1, false -> 0
+
+let isLoggedIn = "no"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 -> true, 0 -> false
+// "" -> false, "string" -> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNumber)
